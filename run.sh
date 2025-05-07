e08389793c7cbe1b2003b15cbc9fbea3522093d3
TrucThanhHoang <125446385+TrucThanhHoang@users.noreply.github.com>
2025-05-07 19:04:09 +0700

    Cập nhật ảnh nhóm 2

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 classes=cat_dog  # the input image
 pretrained_model_path=checkpoints/v1-5-pruned.ckpt
 datapath=datasets/images/$classes
-caption="<new1> cat and <new2> dog"  # the training input prompt
-modifier_token="<new1>+<new2>" # new tokens
-newtoken=2 # the number of new tokens
+caption="ID photo of <new1> Tan, <new2> Thinh, and <new3> Truc"  # the training input prompt
+modifier_token="<new1>+<new2>+<new3>" # new tokens
+newtoken=3 # the number of new tokens
 fine_yaml=full.yaml
 seed=1 
 suffix="full_${seed}"
 name=$classes_${suffix}
 save_path=/content/drive/MyDrive/DisenDiff/$classes
-class_prompt="a cat and a dog"
-
 
 python -u  train.py \
         --base configs/$fine_yaml  \
